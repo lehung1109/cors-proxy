2df42b0024e5fd77fddcc1a0b39f2d639abdc689
Hung Le <hung.le@optimizely.com>
2023-09-19 22:34:04 +0700

    update file

--- api/index.js
+++ api/index.js
@@ -1,14 +1,15 @@
-const app = require('express')();
-const { v4 } = require('uuid');
+import express from 'express';
+
+const app = express();
 
 app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
+  const path = `/api/item/123`;
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.get('/api/item/:slug', (req, res) => {
+app.get('/api/item/123', (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
 });
