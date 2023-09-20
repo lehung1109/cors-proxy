cc20635aa9aedf15859ce2ee644903ef9190c108
Hung Le <hun0895@gmail.com>
2023-09-20 04:02:44 +0000

    automation build code

---
+++ api/index.js
@@ -0,0 +1,2 @@
+import app from "./module/server/index.js";
+export default app;
---
+++ api/module/server/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import proxy from '../proxy/index.js';
+const app = express();
+app.use(proxy);
+app.get('/', (req, res) => {
+    res.redirect('/api');
+});
+app.get('/api', (req, res) => {
+    // add more work
+});
+export default app;
